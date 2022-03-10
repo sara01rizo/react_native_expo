9fdf5d6da325d45fcf7c2f5f37ed8cff54478a0a
sara-pulido <sara01rizo@gmail.com>
2022-03-10 02:11:55 +0100

    Create: second button from TouchableOpacity then we can edit the styles from a Button

--- react_native_expo/App.js
+++ react_native_expo/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TouchableOpacity, Alert } from 'react-native';
 import palmtree from "./assets/palm-tree.jpg";
 
 const App = () => {
@@ -14,7 +14,11 @@ const App = () => {
         color="#000"
         title='Jump in'
         onPress={() => Alert.alert('Hi!!!!!')}
+        style={styles.button}
       />
+      <TouchableOpacity onPress={() => Alert.alert('Hello!!!')}>
+        <Text style={styles.buttonText}>Click me</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -29,7 +33,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   title: {fontSize: 30, color: "#fff"},
-  image: {height: 200, width: 200, borderRadius: 100, marginBottom: 5.5, opacity: 15}
+  image: {height: 200, width: 200, borderRadius: 100, marginBottom: 5.5},
+  button: {
+    backgroundColor: '#00bfff',
+    alignItems: "center",
+    padding: 10,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 20
+  }
 });
 
 export default App;
