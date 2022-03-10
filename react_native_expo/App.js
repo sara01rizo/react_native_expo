cfcaffaa1b884a44db2ea285165817f55c9c0f5e
sara-pulido <sara01rizo@gmail.com>
2022-03-10 01:19:40 +0100

    Add: image using uri and style, from external url

--- react_native_expo/App.js
+++ react_native_expo/App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const App = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hello World!</Text>
+      <Image 
+        source={{uri: "https://i.picsum.photos/id/881/200/300.jpg?hmac=OaIsS2cuxcnUpCVdxcFoc8JwfJgzWEv2Z9F_qEN9tHU"}}
+        style={styles.image}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,9 +19,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    backgroundColor: "#282828",
     justifyContent: 'center',
   },
-  title: {fontSize: 30},
+  title: {fontSize: 30, color: "#fff"},
+  image: {height: 200, width: 200}
 });
 
 export default App;
