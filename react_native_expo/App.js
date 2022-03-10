6891b095a686e32af4c3fcc5da72132352cd15e9
sara-pulido <sara01rizo@gmail.com>
2022-03-10 01:31:30 +0100

    Update: styles for images in react native

--- react_native_expo/App.js
+++ react_native_expo/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import palmtree from "./assets/palm-tree.jpg";
 
 const App = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hello World!</Text>
       <Image 
-        source={{uri: "https://i.picsum.photos/id/881/200/300.jpg?hmac=OaIsS2cuxcnUpCVdxcFoc8JwfJgzWEv2Z9F_qEN9tHU"}}
+        source={palmtree}
         style={styles.image}
       />
       <StatusBar style="auto" />
@@ -23,7 +24,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   title: {fontSize: 30, color: "#fff"},
-  image: {height: 200, width: 200}
+  image: {height: 200, width: 200, borderRadius: 100}
 });
 
 export default App;
